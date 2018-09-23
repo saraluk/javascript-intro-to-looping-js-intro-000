ac00efcd9cbee71a47ab6d031ee7da365b18aa97
saraluk <saralukkai@gmail.com>
2018-09-23 19:31:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,14 @@ function whileLoop(countdown){
   }
   return 'done'
 }
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array){
+  do {
+    array = array.length - 1;
+  } while (array.length > 0 && incrementVariable())
+  return array;
+}
