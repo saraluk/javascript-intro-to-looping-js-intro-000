4c5c5ad0b3d2e21b6f473f3440d0ef2e84da10d4
saraluk <saralukkai@gmail.com>
2018-09-23 19:32:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,7 @@ function incrementVariable() {
 
 function doWhileLoop(array){
   do {
-    array = array.length--;
+    array = array.push();
   } while (array.length > 0 && incrementVariable())
   return array
 }
